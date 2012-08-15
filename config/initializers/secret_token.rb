bb9bcac325b379d75a3ebed0b7d2fc39ebdf6d11
saberma <mahb45@gmail.com>
2012-08-15 09:42:18 +0800

    init rails.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShopqiHomepage::Application.config.secret_token = '019a494af07b70061f9d944219c9944c642adc3b0bd53d0bcaca05a3ee1192c14f843b9ff6201cf95be098b0bed89ee8e30ad4a653ac8a75dc000c84848e913c'
