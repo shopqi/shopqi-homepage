54ac792b277dc2a97bdd93d014581b0f47de39b6
saberma <mahb45@gmail.com>
2012-08-26 18:04:06 +0800

    use new sticky footer, so that we can use margin.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def html_tag( lang = "en", &block ) # http://j.mp/xx2QPw
+    haml_concat Haml::Util::html_safe <<-"HTML".gsub( /^\s+/, '' )
+      <!--[if lt IE 7 ]>              <html lang="#{lang}" class="no-js ie6"> <![endif]-->
+      <!--[if IE 7 ]>                 <html lang="#{lang}" class="no-js ie7"> <![endif]-->
+      <!--[if IE 8 ]>                 <html lang="#{lang}" class="no-js ie8"> <![endif]-->
+      <!--[if IE 9 ]>                 <html lang="#{lang}" class="no-js ie9"> <![endif]-->
+      <!--[if (gt IE 9)|!(IE)]><!-->  <html lang="#{lang}" class="no-js"> <!--<![endif]-->
+    HTML
+    haml_concat capture( &block ) << Haml::Util::html_safe( "\n</html>" ) if block_given?
+  end
+
 end
